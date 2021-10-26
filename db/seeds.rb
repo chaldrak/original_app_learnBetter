4c9201b84166aad54164a6dc07ed013c08fd7fad
chaldrak <chaldrakus@gmail.com>
2021-10-26 14:51:23 +0100

    rails_admin and cancancan and job done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+    name: "admin",
+    email: "chaldrakus@gmail.com",
+    is_admin: true
+)
+Job.create(
+    title: "My first Job",
+    content: "content of my first job. You can congratulate me.",
+    user: user
+)
