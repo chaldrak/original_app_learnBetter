85f4016afd9c64dcd77870a0f6259d1a1ee424ee
chaldrak <chaldrakus@gmail.com>
2021-10-28 19:12:01 +0100

    first changes

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,57 +3,42 @@ require 'rails_helper'
 RSpec.describe 'Fonction de création des utilisateur', type: :system do
 
   describe "Nouvelle fonction de création d'un user" do
+
     context "Lors de la création d'un nouvel user" do
-      it "Avec des données valides un mail de confirmation est envoyé" do
-        visit root_path
-        click_link "Inscription"
-        expect(current_path).to eq(new_user_registration_path)
+      it "Avec une email adress vide" do
+        visit new_user_registration_path
+        fill_in "user[name]", with: "testeur"
+        fill_in "user[email]", with: ""
+        fill_in "user[password]", with: "test-password"
+        fill_in "user[password_confirmation]", with: "test-password"
+        click_button "Inscription"
+        expect(page).to have_content("E-mail doit être rempli(e)")
+      end
+    end
+    
+    context "Lors de la création d'un nouvel user" do
+      it "Avec une confirmation de mot de passe non conforme" do
+        visit new_user_registration_path
         fill_in "user[name]", with: "testeur"
         fill_in "user[email]", with: "tester@example.tld"
         fill_in "user[password]", with: "test-password"
-        fill_in "user[password_confirmation]", with: "test-password"
-        sleep 10
+        fill_in "user[password_confirmation]", with: "new-test-password"
         click_button "Inscription"
-        
-        expect(page).to have_content("Un message avec un lien de confirmation vous a été envoyé par mail. Veuillez suivre ce lien pour activer votre compte.")
+        expect(page).to have_content("Confirmation du mot de passe ne concorde pas avec Mot de passe")
       end
     end
 
-    # context "Lors de la création d'un nouvel user" do
-    #   it "Avec une email adress vide" do
-    #     visit new_user_registration_path
-    #     fill_in "user[name]", with: "testeur"
-    #     fill_in "user[email]", with: ""
-    #     fill_in "user[password]", with: "test-password"
-    #     fill_in "user[password_confirmation]", with: "test-password"
-    #     click_button "Inscription"
-    #     expect(page).to have_content("E-mail doit être rempli(e)")
-    #   end
-    # end
-    
-    # context "Lors de la création d'un nouvel user" do
-    #   it "Avec une confirmation de mot de passe non conforme" do
-    #     visit new_user_registration_path
-    #     fill_in "user[name]", with: "testeur"
-    #     fill_in "user[email]", with: "tester@example.tld"
-    #     fill_in "user[password]", with: "test-password"
-    #     fill_in "user[password_confirmation]", with: "new-test-password"
-    #     click_button "Inscription"
-    #     expect(page).to have_content("Confirmation du mot de passe ne concorde pas avec Mot de passe")
-    #   end
-    # end
-
-    # context "Lors de la création d'un nouvel user" do
-    #   it "Avec un mot de passe trop court" do
-    #     visit new_user_registration_path
-    #     fill_in "user[name]", with: "testeur"
-    #     fill_in "user[email]", with: "tester@example.tld"
-    #     fill_in "user[password]", with: "pass"
-    #     fill_in "user[password_confirmation]", with: "pass"
-    #     click_button "Inscription"
-    #     expect(page).to have_content("Mot de passe est trop court (au moins 6 caractères)")
-    #   end
-    # end
+    context "Lors de la création d'un nouvel user" do
+      it "Avec un mot de passe trop court" do
+        visit new_user_registration_path
+        fill_in "user[name]", with: "testeur"
+        fill_in "user[email]", with: "tester@example.tld"
+        fill_in "user[password]", with: "pass"
+        fill_in "user[password_confirmation]", with: "pass"
+        click_button "Inscription"
+        expect(page).to have_content("Mot de passe est trop court (au moins 6 caractères)")
+      end
+    end
 
   end
 end
